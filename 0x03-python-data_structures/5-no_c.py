5f65eda93e986245ed43204c941c2df26f118e2a
timileyin42 <akanmuibro@gmail.com>
2023-11-04 10:11:38 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,6 +8,6 @@ def no_c(my_string):
 
     for y in range(length):
         if (my_string[y] == 'c' or my_string[y] == 'C'):
-            new_string = new_string[:(x - y)] + my_string[(y + 1):]
+            new_string = new_string[:(y - x)] + my_string[(y + 1):]
             x += 1
     return (new_string)
