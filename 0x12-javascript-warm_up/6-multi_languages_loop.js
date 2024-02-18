d787076578fe44999bcf9ef981ee55f24cad0d24
timileyin42 <akanmuibro@gmail.com>
2024-02-17 19:15:23 -0800

    last_ad

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i of lang) {
-  console.log(i);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
